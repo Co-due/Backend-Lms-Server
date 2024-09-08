825515e5bc8369e32c168aae8fb2df07dc4214d1
GS_song98 <20003204@sju.ac.kr>
2024-09-08 20:34:05 +0900

    [#2]feat: CreateClassRoomRequest에 userId, name 매핑

---
+++ src/main/java/soma/haeya/lms/group/config/argumentresolver/CreateClassroomArgumentResolver.java
@@ -0,0 +1,56 @@
+package soma.haeya.lms.group.config.argumentresolver;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import soma.haeya.lms.group.model.request.CreateClassroomRequest;
+
+@Component
+public class CreateClassroomArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(CreateClassroomRequest.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+        @NonNull MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory
+    ) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        Long userId = extractUserIdFromHeader(request);
+        String name = extractNameFromBody(request);
+
+        return new CreateClassroomRequest(userId, name);
+    }
+
+    private Long extractUserIdFromHeader(HttpServletRequest request) {
+        String userIdHeader = request.getHeader("X-User-Id");
+
+        if (userIdHeader == null || userIdHeader.isEmpty()) {
+            throw new IllegalArgumentException("X-User-Id 헤더가 없습니다.");
+        }
+
+        if (!userIdHeader.matches("[0-9]+")) {
+            throw new IllegalArgumentException("X-User-Id 값이 유효한 숫자가 아닙니다.");
+        }
+
+        return Long.parseLong(userIdHeader);
+    }
+
+    private String extractNameFromBody(HttpServletRequest request) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode root = objectMapper.readTree(request.getInputStream());
+        return root.path("name").asText();
+    }
+}
