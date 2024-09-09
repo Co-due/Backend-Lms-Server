e3cdcf8dbe7371cd5a074cdeae3ce6d6ec735b79
GS_song98 <20003204@sju.ac.kr>
2024-09-09 21:02:43 +0900

    [#4]refactor: ClassroomApiClient -> DbServerApiClient

--- src/main/java/soma/haeya/lms/classroom/service/ClassroomService.java
+++ src/main/java/soma/haeya/lms/classroom/service/ClassroomService.java
@@ -2,20 +2,20 @@ package soma.haeya.lms.classroom.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import soma.haeya.lms.classroom.client.ClassroomApiClient;
 import soma.haeya.lms.classroom.model.request.ClassroomCreateRequest;
+import soma.haeya.lms.common.client.DbServerApiClient;
 
 @Service
 @RequiredArgsConstructor
 public class ClassroomService {
 
-    private final ClassroomApiClient classroomApiClient;
+    private final DbServerApiClient dbServerApiClient;
     private final InviteLinkService inviteLinkService;
 
     public void createClassroom(ClassroomCreateRequest createClassroomRequest) {
         String inviteLink = inviteLinkService.makeLink();
         createClassroomRequest.createInviteLink(inviteLink);
 
-        classroomApiClient.createClassroom(createClassroomRequest);
+        dbServerApiClient.createClassroom(createClassroomRequest);
     }
 }
