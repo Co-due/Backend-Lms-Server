0f87a58d505b696dbe5b8cbd350ac31dfe93f5dd
GS_song98 <20003204@sju.ac.kr>
2024-09-10 17:41:49 +0900

    [#5]feat: RestClient 요청 추가

--- src/main/java/soma/haeya/lms/common/client/DbServerApiClient.java
+++ src/main/java/soma/haeya/lms/common/client/DbServerApiClient.java
@@ -2,9 +2,12 @@ package soma.haeya.lms.common.client;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 import soma.haeya.lms.classroom.model.request.ClassroomCreateRequest;
+import soma.haeya.lms.classroom.model.response.MyClassroomWithCountResponse;
 import soma.haeya.lms.student.model.request.RegisterStudentRequest;
 
 @Component
@@ -16,5 +19,8 @@ public interface DbServerApiClient {
 
     @PostExchange("/student")
     void registerStudent(@RequestBody RegisterStudentRequest registerStudentRequest);
+
+    @GetExchange("/classroom")
+    MyClassroomWithCountResponse getMyClassrooms(@RequestParam Long userId);
 
 }
