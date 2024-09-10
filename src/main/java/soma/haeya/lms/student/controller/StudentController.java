ba40dc4959754872b9ccdcec68388e480ee2c031
GS_song98 <20003204@sju.ac.kr>
2024-09-10 17:23:09 +0900

    [#4]feat: DefaultSuccessResponse -> SuccessResponse

--- src/main/java/soma/haeya/lms/student/controller/StudentController.java
+++ src/main/java/soma/haeya/lms/student/controller/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import soma.haeya.lms.common.config.argumentresolver.UserId;
-import soma.haeya.lms.common.model.response.DefaultSuccessResponse;
+import soma.haeya.lms.common.model.response.SuccessResponse;
 import soma.haeya.lms.student.model.request.RegisterStudentRequest;
 import soma.haeya.lms.student.service.StudentService;
 
@@ -19,11 +19,11 @@ public class StudentController {
     private final StudentService studentService;
 
     @PostMapping
-    public ResponseEntity<DefaultSuccessResponse> registerStudent(
+    public ResponseEntity<SuccessResponse> registerStudent(
         @UserId @RequestBody RegisterStudentRequest registerStudentRequest
     ) {
         studentService.registerStudent(registerStudentRequest);
 
-        return ResponseEntity.ok(new DefaultSuccessResponse("클래스룸에 참여했습니다."));
+        return ResponseEntity.ok(new SuccessResponse("클래스룸에 참여했습니다."));
     }
 }
