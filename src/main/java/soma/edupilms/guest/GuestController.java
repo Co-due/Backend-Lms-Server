766de59c2b4d97044a3734029966276f136d4d57
GS_song98 <20003204@sju.ac.kr>
2024-09-26 15:41:12 +0900

    [#15]refactor: SuccessResponse 적용

--- src/main/java/soma/edupilms/guest/GuestController.java
+++ src/main/java/soma/edupilms/guest/GuestController.java
@@ -9,6 +9,7 @@ import soma.edupilms._config.advice.AccountId;
 import soma.edupilms.guest.models.ClassroomAccountResponse;
 import soma.edupilms.guest.models.RegisterGuestRequest;
 import soma.edupilms.guest.service.GuestService;
+import soma.edupilms.web.models.SuccessResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,11 +18,11 @@ public class GuestController {
     private final GuestService guestService;
 
     @PostMapping("/v1/guest")
-    public ResponseEntity<ClassroomAccountResponse> registerGuest(
+    public ResponseEntity<SuccessResponse> registerGuest(
         @AccountId @RequestBody RegisterGuestRequest registerGuestRequest
     ) {
         ClassroomAccountResponse classroomAccountResponse = guestService.registerGuest(registerGuestRequest);
 
-        return ResponseEntity.ok(classroomAccountResponse);
+        return ResponseEntity.ok(SuccessResponse.withDetailAndResult("게스트 등록에 성공했습니다.", classroomAccountResponse));
     }
 }
