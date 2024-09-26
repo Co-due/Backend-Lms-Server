66ab246dced51179d736d17e825456b9edb6aa24
GS_song98 <20003204@sju.ac.kr>
2024-09-26 15:06:39 +0900

    [#18]refactor: sse와 redis의 순환문제를 sseEmitters를 분리해 해결

--- src/main/java/soma/edupilms/progress/ProgressController.java
+++ src/main/java/soma/edupilms/progress/ProgressController.java
@@ -4,32 +4,32 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import soma.edupilms.progress.models.ActionRequest;
-import soma.edupilms.progress.service.PublishService;
-import soma.edupilms.progress.service.SubscribeService;
+import soma.edupilms.progress.service.service.SseService;
 
 @RestController
 @RequiredArgsConstructor
 public class ProgressController {
 
-    private final SubscribeService subscribeService;
-    private final PublishService publishService;
+    private final SseService sseService;
 
-    @GetMapping(value = "/v1/progress/connect/{classroomId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public ResponseEntity<SseEmitter> connect(@PathVariable String classroomId) {
-        SseEmitter connection = subscribeService.subscribe(classroomId);
+    @GetMapping(value = "/v1/progress/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> connect(@RequestParam String classroomId) {
+        System.out.println("classroomId = " + classroomId);
+        SseEmitter connection = sseService.createOrGetConnection(classroomId);
 
         return ResponseEntity.ok(connection);
     }
 
     @PostMapping("/v1/progress/send")
     public ResponseEntity<Void> send(@RequestBody ActionRequest actionRequest) {
-        publishService.publish(actionRequest);
+        System.out.println("actionRequest.getActionType() = " + actionRequest.getActionType());
+        sseService.sendAction(actionRequest);
 
         return ResponseEntity.noContent().build();
     }
---
+++ src/main/java/soma/edupilms/progress/service/service/SseService.java
@@ -0,0 +1,48 @@
+package soma.edupilms.progress.service.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import soma.edupilms.progress.models.ActionRequest;
+import soma.edupilms.progress.service.service.emitters.SseEmitters;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SseService {
+
+    private final SseEmitters sseEmitters;
+    private final RedisService redisService;
+
+    private static final Long TIME_OUT = 60 * 1000L; // 1분
+
+    public SseEmitter createOrGetConnection(String classroomId) {
+
+        redisService.subscribe(classroomId);
+
+        // sseEmitter 가져오기 없으면 만들어서 반환
+        return sseEmitters.findSseEmitter(classroomId)
+            .orElseGet(() -> createNewSseEmitter(classroomId));
+    }
+
+    public void sendAction(ActionRequest actionRequest) {
+        redisService.publish(actionRequest);
+    }
+
+    private SseEmitter createNewSseEmitter(String classroomId) {
+        final SseEmitter sseEmitter = new SseEmitter(TIME_OUT);
+
+        sseEmitters.create(classroomId, sseEmitter);
+        sseEmitter.onTimeout(sseEmitter::complete);
+        sseEmitter.onError(e -> sseEmitter.complete());
+
+        sseEmitter.onCompletion(() -> {
+            System.out.println("[Sse connection closes] classroomId = " + classroomId);
+            sseEmitters.delete(classroomId);
+            redisService.removeSubscribe(classroomId);
+        });
+
+        return sseEmitter;
+    }
+}
